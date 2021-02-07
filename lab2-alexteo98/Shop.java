619b0dd7c0b4af30a43595adc79d08d1fa60a5aa
alexteo98 <alexteo98@gmail.com>
2021-02-07 12:55:26 +0800

    Documented Shop clas

--- lab2-alexteo98/Shop.java
+++ lab2-alexteo98/Shop.java
@@ -17,8 +17,18 @@ class Shop {
   private Queue q;
 
   // ----- Constructors --------------------
+
+  /**
+   * Constructor for a Shop.
+   *
+   * Initialises the array of Customers and Counters by calling their relevent methods.
+   *
+   * @param noOfCustomers Number of Customers in the shop.
+   * @param noOfCounters Number of Counters in the shop.
+   * @param timings Array used to store arrival and service time of customers.
+   */
+
   public Shop(int noOfCustomers, int noOfCounters, double[][] timings) { 
-    // initialise Customer and counter arrays
     this.allCounters = createCounters(noOfCounters);
     this.allCustomers = createCustomers(noOfCustomers, timings);
     this.noOfCustomers = noOfCustomers;
@@ -27,21 +37,40 @@ class Shop {
 
   // ----- Getter and Setters ---------------------
 
+  /**
+   * Gets the queue object of the shop.
+   *
+   * @return The queue object of the shop.
+   */
   public Queue getQueue() { 
     return this.q;
   }
 
+  /**
+   * Sets the queue object of the shop
+   *
+   * @param q Queue object to link to the shop.
+   */
   public void setQueue(Queue q) { 
     this.q = q;
   }
 
+  /**
+   * Gets the array of Customers in the shop.
+   *
+   * @return The array containing all customer objects in the shop.
+   */
   public Customer[] getCustomers() { 
     return this.allCustomers;
   }
 
   // ----- Methods -----------------------
+  /**
+   * Gets a counter object that is available
+   *
+   * @return A counter object belonging to this shop that is available,  null if none available.
+   */
   public Counter getAvailableCounter() { 
-    // returns an available counter, if none available, return null value 
     for (int i = 0; i < noOfCounters; i++) { 
       if (allCounters[i].available()) { 
         return allCounters[i];
@@ -50,6 +79,11 @@ class Shop {
     return null;
   }
 
+  /**
+   * Checks if there are any available counters in the shop.
+   *
+   * @return True if there is an available counter for use, false otherwise.
+   */
   public boolean counterAvailable() { 
     Counter ctr = getAvailableCounter();
     if (ctr == null) { 
@@ -59,9 +93,14 @@ class Shop {
     }
   }
 
+  /**
+   * Initialise an array of counters.
+   *
+   * @param noOfCounters Number of Counter objects to create.
+   *
+   * @return An array of counters belonging to the shop.
+   */
   private Counter[] createCounters(int noOfCounters) {
-    //  creates and returns an array of counters
-    //  @param noOfCounters =  number of counter objects to return in counter array
     Counter[] allCounters = new Counter[noOfCounters];
     for (int i = 0; i < noOfCounters; i++) { 
       allCounters[i] = new Counter();
@@ -69,9 +108,15 @@ class Shop {
     return allCounters;
   }
 
+  /**
+   * Initialise an array of customers.
+   *
+   * @param noOfCustomers Number of Customer objects to create.
+   * @param timings An containing the arrival and service time of each customer.
+   *
+   * @return An array of customers belonging to the shop.
+   */
   private Customer[] createCustomers(int noOfCustomers, double[][] timings) { 
-    // Creates and returns an array of customers
-    // @param noOfCustomers = number of customer objects to return in customer array
     Customer[] allCustomers = new Customer[noOfCustomers];
     for (int i = 0; i < noOfCustomers; i++) { 
       allCustomers[i] = new Customer(timings[i][0], timings[i][1]);
@@ -79,6 +124,11 @@ class Shop {
     return allCustomers;
   }
 
+  /**
+   * Dequeues and gets the next customer in queue.
+   *
+   * @return The next customer in line.
+   */
   public Customer nextCustomer() { 
     return (Customer) q.deq();
   }
